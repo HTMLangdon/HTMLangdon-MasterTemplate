2f6fadc24803707af0e13dc4e4360c5fe0b11e50
HTMLangdon <HTMLangdon@users.noreply.github.com>
2016-05-04 17:15:44 -0400

    -=initialize file for javascript functionality=-

---
+++ Build/assets/js/initialize.js
@@ -0,0 +1,36 @@
+// initialization of custom components and scripts go here.
+
+/*
+  Load scripts with Require.js, run functions in Document Ready Event Handler.
+*/
+
+
+
+
+function initialize()
+{
+    //check page name var
+    //check user y-position on website with window on scroll event
+    //commit actions based on current area of website.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(e)
+{
+    initialize();
+
+
+
+});
